bbea5cf7d27b7823b45c5b6814200317389745bb
Nancy-techprep <117962831+Nancy-techprep@users.noreply.github.com>
2023-05-03 17:38:07 +0000

    made show page dynamic

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,22 @@ class PhotosController < ApplicationController
      @list_of_photos = matching_photos.order({ :created_at => :desc})
     render( :template => "photo_templates/index.html.erb")
   end
+
+  def show
+    #Parameters: {"path_id"=>":"}
+
+    url_id = params.fetch("path_id")
+      Photo.where({ :id => url_id})
+   # url_username = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id})
+
+    @the_photo = matching_photos.at(0)
+
+    #if @the_user == nil
+    # redirect_to("/404")
+    #else
+      render({ :template => "photo_templates/show.html.erb" })
+    #end
+  end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ get("/users", { :controller => "users", :action => "index"})
 
 get("/users/:path_username", { :controller => "users", :action => "show"})
 
+
+get("/photos/:path_id", { :controller => "photos", :action => "show"})
+
 get("/photos", { :controller => "photos", :action => "index"})
+
 end
