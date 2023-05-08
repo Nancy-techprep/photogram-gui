906e917ff5f039bde5a335ec10c90da414a4c0ff
Nancy-techprep <117962831+Nancy-techprep@users.noreply.github.com>
2023-05-08 14:54:30 +0000

    completed photogram gui

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,33 +1,31 @@
 class PhotosController < ApplicationController
-
   def index
     matching_photos = Photo.all
 
-
-     @list_of_photos = matching_photos.order({ :created_at => :desc})
-    render( :template => "photo_templates/index.html.erb")
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
+    render(:template => "photo_templates/index.html.erb")
   end
 
   def show
     #Parameters: {"path_id"=>":"}
 
     url_id = params.fetch("path_id")
-      Photo.where({ :id => url_id})
-   # url_username = params.fetch("path_id")
+    Photo.where({ :id => url_id })
+    # url_username = params.fetch("path_id")
 
-    matching_photos = Photo.where({ :id => url_id})
+    matching_photos = Photo.where({ :id => url_id })
 
     @the_photo = matching_photos.at(0)
 
     #if @the_user == nil
     # redirect_to("/404")
     #else
-      render({ :template => "photo_templates/show.html.erb" })
+    render({ :template => "photo_templates/show.html.erb" })
     #end
   end
 
   def baii
- # Parameters: {"toast_id"=>"785"}
+    # Parameters: {"toast_id"=>"785"}
     the_id = params.fetch("toast_id")
 
     matching_photos = Photo.where({ :id => the_id })
@@ -35,7 +33,70 @@ class PhotosController < ApplicationController
     the_photo = matching_photos.at(0)
 
     the_photo.destroy
-    
-    render({ :template => "photo_templates/baii.html.erb"})
+
+    #render({ :template => "photo_templates/baii.html.erb"})
+
+    redirect_to("/photos")
   end
-end 
+
+  def create
+    #Parameters: {"query_image"=>"a", "query_caption"=> "b" "query_owner_id"=>"c"}
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id", false)
+
+    a_new_photo = Photo.new
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+    next_url = "/photos/" + a_new_photo.id.to_s
+    redirect_to(next_url)
+    # render({ :template => "photo_templates/create.html.erb" })
+  end
+
+  def update
+    # Parameters: {"query_image"=>"a", "query_caption"=>"b", "modify_id"=>"c"}
+
+    the_id = params.fetch("modify_id")
+
+    matching_photos = Photo.where({ :id => the_id })
+
+    the_photo = matching_photos.first
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+
+    the_photo.image = input_image
+    the_photo.caption = input_caption
+
+    the_photo.save
+
+    #render({ :template => "/photo_templates/update.html.erb" })
+    next_url = "/photos/" + the_photo.id.to_s
+
+    redirect_to(next_url)
+  end
+
+  def insert_comment
+
+    photo_id = params.fetch("input_photo_id")
+    input_author = params.fetch("input_author_id")
+    input_comment = params.fetch("input_body")
+
+    new_comment = Comment.new
+
+    new_comment.author_id = input_author
+    new_comment.photo_id = photo_id
+    new_comment.body = input_comment
+
+    new_comment.save
+
+    next_url = "/photos/" + new_comment.photo_id.to_s
+
+    redirect_to(next_url)
+  end
+   
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
 
 get("/users", { :controller => "users", :action => "index"})
 
+get("/", { :controller => "users", :action => "index" })
+
 get("/users/:path_username", { :controller => "users", :action => "show"})
 
+get("/users/:path_username", { :controller => "users", :action => "show" })
+
+get("/insert_user_record", { :controller => "users", :action => "create_username"})
+
+get("/update_user/:user_id", { :controller => "users", :action => "update_user"})
 
 get("/photos/:path_id", { :controller => "photos", :action => "show"})
 
@@ -11,4 +18,10 @@ get("/photos", { :controller => "photos", :action => "index"})
 
 get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii"})
 
+get("/insert_photo", { :controller => "photos", :action => "create"})
+
+get("/update_photo/:modify_id", { :controller => "photos", :action => "update" })
+
+get("/insert_comment_record", { :controller => "photos", :action => "insert_comment" })
+# get("/update_photo", { :controller => "photos", :action => "update"})
 end
